4d5e0772a9e6a8e42441e03b01c794dfb6a59e8c
Jiefei-Wang <szwjf08@gmail.com>
2020-08-21 18:16:57 +0800

    Fix mount point issue

--- GCSDokan/globalVariables.cpp
+++ GCSDokan/globalVariables.cpp
@@ -8,6 +8,9 @@ short verbose_level = 0;
 
 void set_mount_point(std::wstring path) {
 	mount_point = remove_tailing_symbol(remove_tailing_slash(to_linux_slash(path)),':');
+	if (mount_point.find(L'/') == string::npos) {
+		mount_point = mount_point + L":";
+	}
 }
 
 std::wstring& get_mount_point() {
